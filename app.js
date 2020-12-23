54a99b06df66111e685a922c98f817a3011f94d9
Jonathan Carlson <JonathanCarlson@Jonathans-MacBook-Pro-2.local>
2020-12-23 00:14:20 -0800

    update with different iterations

--- app.js
+++ app.js
@@ -7,6 +7,15 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
+// import SpotifyPlayer from 'react-spotify-web-playback';
+// import React from 'react';
+// import express from 'express';
+// import request from 'request';
+// import cors from 'cors';
+// import querystring from 'querystring';
+// import cookieParser from 'cookieparser';
+
+
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
@@ -17,6 +26,13 @@ var client_id = '8c5ec548a1b54161a9dc2df93984bd46'; // Your client id
 var client_secret = '72fb5bf689c84b0f9e97174846847c37'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
+
+// var script = document.createElement('script');
+// script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+// script.type = 'text/javascript';
+// var script = document.createElement('script');
+// script.src = 'https://sdk.scdn.co/spotify-player.js';
+// script.type = 'text/javascript';
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -56,8 +72,6 @@ function getRandomSearch() {
   return randomSearch;
 }
 
-
-
 var stateKey = 'spotify_auth_state';
 
 var app = express();
@@ -72,7 +86,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email streaming';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -112,11 +126,13 @@ app.get('/callback', function(req, res) {
       json: true
     };
 
+    var uri = '';
+
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
 
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+        access_token = body.access_token,
+        refresh_token = body.refresh_token;
 
         // var options = {
         //   url: 'https://api.spotify.com/v1/me',
@@ -141,13 +157,14 @@ app.get('/callback', function(req, res) {
           json: true,
         };
 
-        request.get(search, function(error, response, body) {
+        request.get(search, function (error, response, body) {
           console.log(body);
           console.log('printing out track');
           console.log(body.tracks.items[0].uri);
+          uri = body.tracks.items[0].uri;
           console.log(error);
           // console.log(response);
-          
+
         });
 
         // we can also pass the token to the browser to make requests from there
@@ -189,6 +206,14 @@ app.get('/refresh_token', function(req, res) {
     }
   });
 });
+
+// ReactDOM.render(
+//   <SpotifyPlayer
+//     token=""
+//     uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
+//   />;
+//   document.getElementById('root');
+// );
 
 console.log('Listening on 8888');
 app.listen(8888);
